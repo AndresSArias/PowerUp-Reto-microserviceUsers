ec746d6caf6a90a51b3672cd79f5ac0beb67f977
AndresSArias <andres31321@gmail.com>
2023-05-31 01:39:55 -0500

    [Update] permiss of roles for endpoints

--- src/main/java/com/pragma/powerup/usermicroservice/configuration/security/MainSecurity.java
+++ src/main/java/com/pragma/powerup/usermicroservice/configuration/security/MainSecurity.java
@@ -51,8 +51,8 @@ public class MainSecurity {
         http.cors().and().csrf().disable()
                 .authorizeRequests(requests -> requests
                         .requestMatchers("/auth/login","/auth/refresh", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/user/getUser/{numberDocument}").hasRole("OWNER")
-                        .requestMatchers("/user/createUserOwner","/user/getUser/**").hasRole("ADMIN")
+                        .requestMatchers("/user/createUserOwner").hasRole("ADMIN")
+                        .requestMatchers("/user/getUser/{numberDocument}").hasAnyRole("OWNER", "ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin().disable()
